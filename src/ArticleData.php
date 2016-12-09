9503c4d1f2dd3ec8e918dafbd46b8841fc4f9495
Jakob <jakob.vogt@bo-bremen.de>
2016-12-09 07:09:52 +0100

    Add composite key

--- src/ArticleData.php
+++ src/ArticleData.php
@@ -3,14 +3,20 @@
 namespace Sixoquent;
 
 use Illuminate\Database\Eloquent\Model;
+use Mpociot\HasCompositeKey\HasCompositeKey;
 
 class ArticleData extends Model
 {
+    use HasCompositeKey;
+    
     protected $table = 'sixcms_article_data';
     protected $hidden = ['sindex'];
     public $timestamps = false;
+    protected $fillable = ['article_id', 'value'];
+    protected $primaryKey = ['article_id', 'fieldname'];
     
     public function article(){
         return $this->belongsTo('\Sixoquent\Article');
     }
+    
 }
