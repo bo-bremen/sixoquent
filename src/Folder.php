261f9a26649e5fded5fab548b6a2746b53722a21
Jakob <jakob.vogt@bo-bremen.de>
2017-09-21 08:58:21 +0200

    Folder issues

--- src/Folder.php
+++ src/Folder.php
@@ -9,15 +9,15 @@ class Folder extends Model
 	protected $table = 'sixcms_folder';
 	
 	public function parentFolder(){
-		return $this->belongsTo('Folder', 'parent_id');
+		return $this->belongsTo('Sixoquent\Folder', 'parent_id');
 	}
 	
 	public function childrenFolder(){
-		return $this->hasMany('Folder', 'parent_id');
+		return $this->hasMany('Sixoquent\Folder', 'id', 'parent_id');
 	}
 	
 	public function areas(){
-		return $this->hasMany('Area');
+		return $this->hasMany('Sixoquent\Area');
 	}
 	
 }
